198b0e20f8ffa8e1c75c11a933dbf283c93af94f
Lucas J Sola <lucas.sola.85@gmail.com>
2024-05-08 09:28:38 -0300

    add auth by owner
    list todos by owners

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,11 +7,12 @@ specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-    })
-    .authorization((allow) => [allow.publicApiKey()]),
+  Todo: a.model({
+    content: a.string(),
+
+  })
+  //auth with owner
+  .authorization((auth)=>[auth.owner("userPools")]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -19,7 +20,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
+    defaultAuthorizationMode: "userPool",
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
     },
--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function App() {
     <Authenticator>
       {({ signOut, user}) => (
         <main>
-          <h1>My todos</h1>
+          <h1>Todos List of 👉 {user?.signInDetails?.loginId?.slice(0, user.signInDetails.loginId.indexOf('@'))}!</h1>
           <button onClick={createTodo}>+ new</button>
           <ul>
             {todos.map((todo) => (
